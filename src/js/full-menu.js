3685f559fd6a416bc73bbcf16292443a1e085993
0xfff <0xfff@protonmail.com>
2016-11-01 02:10:13 +0100

    [Feature] Mobile Nav + Burger / CTA on Home

--- src/js/full-menu.js
+++ src/js/full-menu.js
@@ -2,9 +2,27 @@
 
 	$(function() {
 
-		$('#toggle').click(function() {
-			$(this).toggleClass('active');
-			$('#overlay').toggleClass('open');
+		var isOpen = false,
+			$overlay = $('[data-js-overlay-menu]'),
+			$burger = $('[data-js-open-menu]');
+
+		$burger.click(function() {
+			var $this = $(this);
+
+			isOpen = !isOpen;
+
+			$this.toggleClass('is-active', isOpen);
+			$overlay.toggleClass('is-open', isOpen);
+
+			if (isOpen) {
+				window.noScroll.on();
+				$overlay.attr('aria-hidden', 'false');
+				$burger.attr('aria-expanded', 'true');
+			} else {
+				window.noScroll.off();
+				$overlay.attr('aria-hidden', 'true');
+				$burger.attr('aria-expanded', 'false');
+			}
 		});
 
 	});
