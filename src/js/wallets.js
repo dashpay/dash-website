5d3388abf29fbdc7402b8c36e9aaf99e08ed1e3e
Chuck Williams <williams@playerlync.com>
2017-01-17 01:13:38 -0700

    WIP - wallet page updates; adds wallet product modals, improves wallet-collection data;

--- src/js/wallets.js
+++ src/js/wallets.js
@@ -2,13 +2,11 @@
  * Created by cwilliams on 1/9/17.
  */
 
-;(function($) {
-  'use strict';
-
-  $(document).ready(function() {
-    $('.wallet-tile a').click(function(event){
-      event.stopImmediatePropagation();
-    })
-  });
-
+;(function ($) {
+	'use strict';
+	$(document).ready(function () {
+		$('.wallet-tile a').click(function (event) {
+			event.stopImmediatePropagation();
+		});
+	});
 }(jQuery));
