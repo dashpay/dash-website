1e19c7640439a1187affa2a43e779cd5cdd11136
Chuck Williams <chuckwilliams37@gmail.com>
2017-02-06 22:46:29 -0700

    Cleanup bad url and javascript to pass build.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,15 +1,16 @@
 ;(function ($) {
 	'use strict';
 	$(document).ready(function () {
-		$('.reset').on('click',function () {
-			var name = $('#contact_name').val();
-			var	email = $('#contact_email').val();
-			var category = $('#contact_category').val();
-		    var message = $('#contact_message').val();
-			console.log(name,email,category,message);
+		$('.reset').on('click', function () {
+			var name = $('#contact_name').val(),
+				email = $('#contact_email').val(),
+				category = $('#contact_category').val(),
+				message = $('#contact_message').val();
+
+			console.log(name, email, category, message);
 			$(this).closest('form').find('input[type=text], textarea').val('');
 			$('#msg_sent').css('display', 'inline-block');
-			$('#msg_sent' ).fadeOut(3000);
+			$('#msg_sent').fadeOut(3000);
 		});
 	});
 }(jQuery));
