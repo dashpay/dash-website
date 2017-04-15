6f5b9fb528099d21dea3c7ab85daab9402596b94
moocowmoo <moo@dash.org>
2017-04-15 22:38:55 +0000

    bugfix - repair mixed-content ssl warnings -- self-host non-ssl image

---
+++ script/cron-refresh_masternode_count_image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# cron-refresh_masternode_count_image.sh
+# pulls, resizes, and places an image from service that hasn't installed ssl
+# self-hosting the content avoids mixed-content/insecure browser warnings
+
+# source image is updated once an hour.
+# production crontab config. check every 10 past so it's reasonably fresh
+#
+# 10 * * * * /home/ubuntu/build/dash-website/script/cron-refresh_masternode_count_image.sh
+#
+
+FILEPATH=/tmp/masternode_count.png
+rm -f $FILEPATH
+wget -q http://178.254.23.111/~pub/masternode_count.png -O $FILEPATH
+
+if [[ -e $FILEPATH ]]; then
+  convert /tmp/masternode_count.png -crop 813x525+6+6 /tmp/masternode_count-crop-813x525.png
+  cp -f /tmp/masternode_count-crop-813x525.png /home/ubuntu/broadcast/assets/img/masternode_count-crop-813x525.png
+fi
