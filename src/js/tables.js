4ccf18a881601827a6030b671067014959beaded
Chuck Williams <williams@playerlync.com>
2017-05-04 03:05:02 -0600

    JS cleanup: moved format Currency & formatNumber functions together
    with network tables JS

--- src/js/tables.js
+++ src/js/tables.js
@@ -113,6 +113,17 @@
 		}
 
 	});
+
+	function formatNumber(num) {
+		return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+	}
+
+	function formatCurrency(num,decimal) {
+		var _decimal;
+
+		_decimal = typeof decimal !== 'undefined' ? decimal : 2;
+		return parseFloat(num).toFixed(_decimal).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+	}
 }(jQuery));
 
 
