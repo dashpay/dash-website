b5d4345f3f5be875142463e9a6baa5739740ea7f
Nathan Marley <nathan.marley@gmail.com>
2017-09-30 19:38:23 -0700

    spell rsync correctly

--- script/deploy-rsync.sh
+++ script/deploy-rsync.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-rync -vzrlptD --delete-after $TRAVIS_BUILD_DIR/_site/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+rsync -vzrlptD --delete-after $TRAVIS_BUILD_DIR/_site/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
